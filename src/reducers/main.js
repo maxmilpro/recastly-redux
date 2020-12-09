821e40e057eeb57efd85af85ab7aebe3cd08dbed
Shane Sutro <shanesutro@gmail.com>
2020-12-08 21:23:59 -0700

    Working through rootReducer

--- src/reducers/main.js
+++ src/reducers/main.js
@@ -2,7 +2,13 @@ import { combineReducers } from 'redux';
 import currentVideo from './currentVideo.js';
 import videoList from './videoList.js';
 
-var rootReducer = () => {};
+var rootReducer = (state, action) => {
+  combineReducers({
+    videos: videoList,
+    currentVideo
+  });
+};
+
 
 //TODO: define the root reducer for this app
 
