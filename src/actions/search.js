e45d03f3bbc5c0bd06f94f89dcfbac068f1601fc
Shane Sutro <shanesutro@gmail.com>
2020-12-08 19:41:38 -0700

    Complete defining action creators

--- src/actions/search.js
+++ src/actions/search.js
@@ -5,8 +5,15 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
- 
-  //TODO:  Write an asynchronous action to handle a video search!
+  return (dispatch) => {
+    searchYouTube({
+      key: YOUTUBE_API_KEY,
+      query: q,
+      max: 5
+    }, (videos) => {
+      dispatch(changeVideoList(videos));
+    });
+  };
 };
 
 export default handleVideoSearch;
